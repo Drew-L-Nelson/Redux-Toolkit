649833017101afb7f4fdaac618deeacdfed5a8c0
Drew-L-Nelson <90492724+Drew-L-Nelson@users.noreply.github.com>
2023-03-30 15:01:28 -0700

    Counter rendering in browser

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Counter from './features/counter/Counter';
 
 export default function App() {
   return (
-    <div>
-      <h1>Redux Toolkit</h1>
-    </div>
+    <main>
+      <Counter />
+    </main>
   );
 }
