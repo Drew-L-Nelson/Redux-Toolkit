1e17904d04399f5ead52014648e381f7aa48fb36
Drew-L-Nelson <90492724+Drew-L-Nelson@users.noreply.github.com>
2023-03-31 15:01:56 -0700

    2 files modified

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import { useSelector } from 'react-redux';
+import { selectAllPosts } from './postsSlice';
 
 const PostsList = () => {
-  const posts = useSelector(state => state.posts)
+  const posts = useSelector(selectAllPosts)
 
   const renderedPosts = posts.map(post => (
     <article>
-      <h3>{post.id}</h3>
+      <h3>{post.id}: {post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
     </article>
   ))
