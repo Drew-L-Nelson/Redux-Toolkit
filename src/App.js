5bc416b646cdaa248f4cb7f73f9659c72c569cbd
Drew-L-Nelson <90492724+Drew-L-Nelson@users.noreply.github.com>
2023-03-31 11:59:28 -0700

    PostsList done - 2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Counter from './features/counter/Counter';
+import PostsList from './features/posts/PostsList'
 
 export default function App() {
   return (
     <main className="App">
-      
+      <PostsList />
     </main>
   );
 }
