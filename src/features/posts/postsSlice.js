fbaec9df644f55272de92e8abf95bdd7f07cbd0e
Drew-L-Nelson <90492724+Drew-L-Nelson@users.noreply.github.com>
2023-05-02 15:47:48 -0700

    reactionAdded reducer added

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -6,13 +6,27 @@ const initialState = [
     id: '1',
     title: 'Learning Redux Toolkit',
     content: "I've heard good things.",
-    date: sub(new Date(), { minutes: 10 }).toISOString()
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsup: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0
+    }
   },
   {
     id: '2',
     title: 'Slices...',
     content: 'The more I say slice, the more I want pizza.',
-    date: sub(new Date(), { minutes: 5 }).toISOString()
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumbsup: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0
+    }
   },
 ];
 
@@ -31,11 +45,25 @@ const postsSlice = createSlice({
             title,
             content,
             date: new Date().toISOString(),
-            userId
+            userId,
+            reactions: {
+              thumbsup: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0
+            }
           },
         };
       },
     },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload
+      const existingPost = state.find(post => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
+      }
+    }
   },
 });
 
