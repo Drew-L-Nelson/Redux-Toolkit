32615388dcedc24206101f75c36bce5efb9e1e58
Drew-L-Nelson <90492724+Drew-L-Nelson@users.noreply.github.com>
2023-05-03 14:32:04 -0700

    extraReducers(builder) added

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -7,17 +7,17 @@ const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
 const initialState = {
   posts: [],
   status: 'idle', //'idle' | 'loading' | 'succeeded' | 'failed'
-  error: null
-}
+  error: null,
+};
 
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
   try {
-    const response = await axios.get(POSTS_URL)
+    const response = await axios.get(POSTS_URL);
     return [...response.data];
   } catch (err) {
     return err.message;
   }
-})
+});
 
 const postsSlice = createSlice({
   name: 'posts',
@@ -40,20 +40,49 @@ const postsSlice = createSlice({
               wow: 0,
               heart: 0,
               rocket: 0,
-              coffee: 0
-            }
+              coffee: 0,
+            },
           },
         };
       },
     },
     reactionAdded(state, action) {
-      const { postId, reaction } = action.payload
-      const existingPost = state.posts.find(post => post.id === postId)
+      const { postId, reaction } = action.payload;
+      const existingPost = state.posts.find((post) => post.id === postId);
       if (existingPost) {
-          existingPost.reactions[reaction]++
+        existingPost.reactions[reaction]++;
       }
-  }
+    },
   },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        // Adding date and reactions
+        let min = 1;
+        const loadedPosts = action.payload.map(post => {
+          post.date = sub(new Date(), { minutes: min++ }).toISOString(),
+          post.reactions = {
+            thumbsUp: 0,
+            hooray: 0,
+            heart: 0,
+            rocket: 0,
+            eyes: 0
+          }
+          return post;
+        });
+
+        // Add any fetched posts to the array
+        state.posts = state.posts.concat(loadedPosts)
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.error.message
+      })
+  }
 });
 
 export const selectAllPosts = (state) => state.posts.posts;
