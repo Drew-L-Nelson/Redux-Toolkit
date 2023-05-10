b380962bdd5509a5eb7d2ac7fc47922dd9731904
Drew-L-Nelson <90492724+Drew-L-Nelson@users.noreply.github.com>
2023-05-10 14:17:40 -0700

    full 'post' return component built

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -19,5 +19,15 @@ const SinglePostPage = () => {
     )
   }
 
-  
+  return (
+    <article>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      <p className='postCredit'>
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+      </p>
+      <ReactionButtons post={post} />
+    </article>
+  )
 }
